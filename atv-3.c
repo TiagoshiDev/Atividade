a287499fa5c81de390f54e16b438f42a008a1064
TiagoshiDev <tiago.tocha@escola.pr.gov.br>
2025-03-12 12:26:48 -0300

    Add files via upload

---
+++ atv-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int ano;
+    scanf("%d", &ano);
+    if (((ano % 4 == 0) && !(ano % 100 == 0)) || (ano % 400 == 0)){
+        printf("bissexto");    
+    } else {
+        printf("nao eh bissexto");
+    }
+    return 0;
+}
